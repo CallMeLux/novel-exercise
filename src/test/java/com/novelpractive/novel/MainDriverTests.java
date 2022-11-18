ee5633fdbd75b658a4784e0ac96c8a1b50c7e58d
CallMeLux <Jourdanjeff@yahoo.com>
2022-11-17 19:50:15 -0500

    initialize repo

---
+++ src/test/java/com/novelpractive/novel/MainDriverTests.java
@@ -0,0 +1,13 @@
+package com.novelpractive.novel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MainDriverTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
